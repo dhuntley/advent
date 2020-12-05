bec7c080410f077795e815dfaf9e6d9c6446eaed
Daniel Huntley <daniel_huntley@intuit.com>
2020-12-05 14:00:45 -0700

    December 4

---
+++ src/puzzles/Day5A.java
@@ -0,0 +1,56 @@
+package puzzles;
+
+import org.apache.commons.lang3.Range;
+import util.Utilities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Day5A {
+    private static class BoardingPass {
+        public int row;
+        public int seat;
+
+        public BoardingPass(String line) {
+            char[] rowInput = line.substring(0,7).toCharArray();
+            char[] seatInput = line.substring(7,10).toCharArray();
+
+            Range<Integer> rowRange = Range.between(0, 127);
+            Range<Integer> seatRange = Range.between(0, 7);
+
+            for (char letter : rowInput) {
+                rowRange = narrowRange(rowRange, letter == 'F');
+            }
+
+            for (char letter : seatInput) {
+                seatRange = narrowRange(seatRange, letter == 'L');
+            }
+
+            row = rowRange.getMinimum();
+            seat = seatRange.getMinimum();
+         }
+
+        private Range<Integer> narrowRange(Range<Integer> initialRange, boolean keepFirstHalf) {
+            int initialMin = initialRange.getMinimum();
+            int initialMax = initialRange.getMaximum();
+            int initialMedian = initialMin + (initialMax - initialMin) / 2;
+
+            return keepFirstHalf ? Range.between(initialMin, initialMedian) : Range.between(initialMedian + 1, initialMax);
+        }
+
+        public int getSeatId() {
+            return row * 8 + seat;
+        }
+    }
+
+    public static void main(String[] args) {
+        Stream<String> lines = Utilities.readLineStream();
+        List<BoardingPass> boardingPasses = new ArrayList<>();
+
+        lines.forEach(line -> boardingPasses.add(new BoardingPass(line)));
+
+        System.out.println(boardingPasses.stream().mapToInt(pass -> pass.getSeatId()).max());
+    }
+}
